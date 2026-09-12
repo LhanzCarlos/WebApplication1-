6cf2d726642b968517a2e276287c56731353961d
COLLEGELAB-06 <COLLEGELAB-06@COLLEGELAB-06>
2026-09-12 17:13:27 +0800

    Add project files.

---
+++ WebApplication1/Models/DTO/ProductDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication1.Models.Domain
+{
+
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int StockQuantity { get; set; }
+    }
+}
